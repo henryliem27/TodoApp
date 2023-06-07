3790aefdd655450631ce194ebafcafc2bbcebfb1
henryliem27 <henryliem27@gmail.com>
2023-06-07 11:28:38 +0700

    binding

--- app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
+++ app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
@@ -4,20 +4,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.ImageView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
+import com.example.todoapp.databinding.TodoItemLayoutBinding
 import com.example.todoapp.model.Todo
 
 class TodoListAdapter(val todoList:ArrayList<Todo>,val adapterOnClick: (Todo)->Unit) :
-    RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
-    class TodoViewHolder(var view: View): RecyclerView.ViewHolder(view)
+    RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>(),TodoItemLayoutInterface {
+    class TodoViewHolder(var view: TodoItemLayoutBinding): RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.todo_item_layout, parent, false)
-
+        val view = TodoItemLayoutBinding.inflate(inflater,parent,false)
         return TodoViewHolder(view)
 
     }
@@ -27,20 +28,21 @@ class TodoListAdapter(val todoList:ArrayList<Todo>,val adapterOnClick: (Todo)->U
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        var checktask = holder.view.findViewById<CheckBox>(R.id.checkTask)
-        checktask.text = todoList[position].title
-        checktask.isChecked = false
-        checktask.setOnCheckedChangeListener{ compoundButton, _ ->
-            if(compoundButton.isChecked){
-                adapterOnClick(todoList[position])}
-            }
-        var imgEdit = holder.view.findViewById<ImageView>(R.id.imgEdit)
-        imgEdit.setOnClickListener{
-            val action = TodoListFragmentDirections.actionEditTodo(todoList[position].uuid)
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.view.todo = todoList[position]
+        holder.view.checkListener = this
+//        var checktask = holder.view.findViewById<CheckBox>(R.id.checkTask)
+//        checktask.text = todoList[position].title
+//        checktask.isChecked = false
+//        checktask.setOnCheckedChangeListener{ compoundButton, _ ->
+//            if(compoundButton.isChecked){
+//                adapterOnClick(todoList[position])}
+//            }
+//        var imgEdit = holder.view.findViewById<ImageView>(R.id.imgEdit)
+//        imgEdit.setOnClickListener{
+//            val action = TodoListFragmentDirections.actionEditTodo(todoList[position].uuid)
+//            Navigation.findNavController(it).navigate(action)
+//        }
     }
-
 
     fun updateTodoList(newTodoList: List<Todo>) {
         todoList.clear()
@@ -48,4 +50,14 @@ class TodoListAdapter(val todoList:ArrayList<Todo>,val adapterOnClick: (Todo)->U
         notifyDataSetChanged()
     }
 
+    override fun OnCheckedChange(cb: CompoundButton, isChecked: Boolean, obj: Todo) {
+        if(isChecked) {
+            adapterOnClick(obj)
+        }
+    }
+
+    override fun onTodoEditClick(v: View) {
+        val action = TodoListFragmentDirections.actionEditTodo(v.tag.toString().toInt())
+            Navigation.findNavController(v).navigate(action)
+    }
 }
